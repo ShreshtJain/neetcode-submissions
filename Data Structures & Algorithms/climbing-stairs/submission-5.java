a1710ebda72e655ad7a27d4016d637b7d6cef2a9
ShreshtJain <52520705+ShreshtJain@users.noreply.github.com>
2026-08-16 14:50:53 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.java
@@ -0,0 +1,22 @@
+//Memoization
+class Solution {
+    int dp[];
+    public int climbStairs(int n) {
+        dp=new int[n+1];
+
+        return solve(n);
+    }
+
+    int solve(int n)
+    {
+        if(n==1||n==0)
+            return 1;
+        
+        if(dp[n]!=0)
+            return dp[n];
+        
+        dp[n]= solve(n-1)+solve(n-2);
+
+        return dp[n];
+    }
+}
