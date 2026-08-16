597c60f8938e6875791edcf7b5e5ea95fa68a992
ShreshtJain <52520705+ShreshtJain@users.noreply.github.com>
2026-08-16 18:16:00 +0530

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.java
@@ -0,0 +1,46 @@
+//Memoization
+class Solution {
+    int n;
+    int[] dp;
+    int[] nums;
+
+    public int rob(int[] nums) {
+        this.n =nums.length;
+        this.dp = new int[n];
+        this.nums=nums;
+
+       if(n<0)
+            return 0;
+        
+        if(n==1)
+            return nums[0];
+        
+        //exclude last house and include first house
+        Arrays.fill(dp,-1);
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        int case1= solve(n-2);
+        
+        //include last house and exclude first house
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        dp[1] = nums[1];
+        int case2 =solve(n-1);
+
+        return Math.max(case1, case2);
+    }
+
+    public int solve(int n)
+    {
+        if(n<0)
+            return 0;
+        
+        if(dp[n]!=-1)
+            return dp[n];
+
+        return dp[n] = Math.max(solve(n-1), solve(n-2) + nums[n]);
+
+    }
+}
